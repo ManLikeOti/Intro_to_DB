321e39ed087604aa98ea8e6b3f9d6efb5a2c34c5
ManLikeOti <anigboguotuto@gmail.com>
2025-02-02 22:35:44 +0100

    SQL connection

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -9,37 +9,59 @@ myalxdb = mysql.connector.connect(host="localhost",
                                 )
 
 cursor = myalxdb.cursor()
-SHOW DATABASES;
-USE alx_book_store;
-CREATE TABLE Authors(
-    author_id INT PRIMARY KEY NOT NULL,
-    author_name VARCHAR(215) NOT NULL
-);
-CREATE TABLE Books(
-    book_id INT PRIMARY KEY NOT NULL,
-    title VARCHAR(130) NOT NULL,
-    author_id INT NOT NULL,
-    price DOUBLE(10, 2) NOT NULL,
-    publication_date DATE,
-    Foreign Key (author_id) REFERENCES Authors(author_id)
-);
-CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY NOT NULL,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL,
-    address TEXT NOT NULL
-);
-CREATE TABLE Orders(
-    order_id INT PRIMARY KEY NOT NULL,
-    customer_id INT NOT NULL,
-    Order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-);
-CREATE TABLE Order_details(
-    orderdetailid INT PRIMARY KEY NOT NULL,
-    quantity DOUBLE NOT NULL,
-    order_id INT NOT NULL,
-    book_id INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
-);
+cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+print("Database Created Successfully!")
+print(myalxdb.get_server_info())
+
+cursor.execute("""
+                CREATE TABLE Authors(
+                    author_id INT PRIMARY KEY NOT NULL,
+                    author_name VARCHAR(215) NOT NULL
+                    )
+              """)
+
+cursor.execute("""
+               CREATE TABLE Books (
+                    book_id INT PRIMARY KEY NOT NULL,
+                    title VARCHAR(130) NOT NULL,
+                    author_id INT NOT NULL,
+                    price DOUBLE(10, 2) NOT NULL,
+                    publication_date DATE,
+                    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+                )
+               """)
+
+cursor.execute("""
+                CREATE TABLE Customers(
+                    customer_id INT PRIMARY KEY NOT NULL,
+                    customer_name VARCHAR(215) NOT NULL,
+                    email VARCHAR(215) NOT NULL,
+                    address TEXT NOT NULL
+                )
+                """)
+
+
+cursor.execute("""
+                CREATE TABLE Orders(
+                    order_id INT PRIMARY KEY NOT NULL,
+                    customer_id INT NOT NULL,
+                    order_date DATE,
+                    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+                    )
+            """)
+
+
+cursor.execute("""
+                CREATE TABLE Order_Details(
+                    orderdetailid INT PRIMARY KEY NOT NULL,
+                    quantity DOUBLE NOT NULL,
+                    order_id INT NOT NULL,
+                    book_id INT NOT NULL,
+                    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+                    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+                )
+        """)
+
+print("Created all tables successfully!")
+cursor.close()
+myalxdb.close()
